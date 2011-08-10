44dd3ba28f54614d7aba4bc58714abcedd23f8c4
Daniel Flood <danielpflood@gmail.com>
2011-08-10 15:50:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MolassesMusic::Application.config.secret_token = '7c720eabfa0a7b3de45f812040722356ce7e450c77ce4e2863c2acf51dd3f88a8ae95771d97a049a11b5980129b654040de9dc359c9b75f2464cc48d06644c68'
