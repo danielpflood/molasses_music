2f633992749f059fb30da54de6c4bb399f98190c
Daniel Flood <danielpflood@gmail.com>
2011-08-18 17:15:47 -0400

    removed updated_at from avatar migration

--- db/migrate/20110818160812_add_avatar_columns_to_user.rb
+++ db/migrate/20110818160812_add_avatar_columns_to_user.rb
@@ -3,11 +3,9 @@ class AddAvatarColumnsToUser < ActiveRecord::Migration
     add_column :users, :avatar_file_name, :string
     add_column :users, :avatar_content_type, :string
     add_column :users, :avatar_file_size, :integer
-    add_column :users, :avatar_updated_at, :datetime
   end
 
   def self.down
-    remove_column :users, :avatar_updated_at
     remove_column :users, :avatar_file_size
     remove_column :users, :avatar_content_type
     remove_column :users, :avatar_file_name
--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(:version => 20110818160812) do
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
